c10b36813b0497f4d27bc4ce8585a72920bf71fa
Seth Forsgren <forsgren.seth@gmail.com>
2022-12-12 09:41:50 -0800

    Making paused button pop for first user
    several didn't see it right away on first land

--- components/Pause.tsx
+++ components/Pause.tsx
@@ -22,7 +22,7 @@ export default function Pause({
 
   var classNameCondition = ""
   if (paused) {
-    classNameCondition="animate-pulse fixed z-20 top-4 right-4 md:top-8 md:right-8 bg-sky-400 w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-white text-2xl hover:bg-sky-500 focus:ring-4 focus:outline-none focus:ring-sky-600 hover:drop-shadow-2xl"
+    classNameCondition="animate-pulse fixed z-20 top-4 right-4 md:top-8 md:right-8 w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-white text-2xl bg-red-500 hover:bg-red-600 ring-4 ring-red-700 focus:outline-none hover:drop-shadow-2xl"
   } else {
     classNameCondition="fixed z-20 top-4 right-4 md:top-8 md:right-8 bg-slate-100 w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-sky-900 text-2xl hover:text-white hover:bg-sky-600 hover:drop-shadow-2xl"
   }
