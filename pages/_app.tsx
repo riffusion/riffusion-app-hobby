384b05df51a589f50ee75be953c6be28cd889b02
Hayk Martiros <hayk.mart@gmail.com>
2022-12-12 11:26:43 -0800

    Add vercel audience

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
