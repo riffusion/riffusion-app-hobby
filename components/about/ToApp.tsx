c45fce2c2f20d1be80d990b808169caef71ac6bd
Seth Forsgren <forsgren.seth@gmail.com>
2022-12-14 02:05:28 -0800

    tweak

--- components/about/ToApp.tsx
+++ components/about/ToApp.tsx
@@ -20,14 +20,14 @@ export default function ToApp() {
     <>
       <button
         type="button"
-        className="fixed z-10 top-8 right-8 md:top-12 md:right-12 text-white bg-gradient-to-br from-purple-600 to-sky-500 hover:bg-gradient-to-bl w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-2xl hover:text-white hover:bg-sky-600 hover:drop-shadow-2xl"
+        className="fixed z-10 top-4 right-6 md:top-12 md:right-12 text-white bg-gradient-to-br from-purple-600 to-sky-500 hover:bg-gradient-to-bl w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-2xl hover:text-white hover:bg-sky-600 hover:drop-shadow-2xl"
         onClick={() => window.location.href = "/"}
       >
         🎸
       </button>
 
       {showButton && (
-        <div className="fixed z-20 top-12 right-8 md:top-16 md:right-12 text-white bg-gradient-to-br w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-2xl">
+        <div className="fixed z-20 top-8 right-6 md:top-16 md:right-12 text-white bg-gradient-to-br w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-2xl">
           <span className="absolute bottom-0 -right-2 bg-white text-black rounded-full p-1 pr-2 pl-2 text-xs">Try it!</span>
           <button
             type="button"
