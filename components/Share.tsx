505d167869324a6fc5b11912d27a5ca74348956a
Seth Forsgren <forsgren.seth@gmail.com>
2022-12-14 22:58:49 -0800

    cursor and hover behavior on share

--- components/Share.tsx
+++ components/Share.tsx
@@ -228,13 +228,13 @@ export default function Share({
                       <span className="w-4/5">Share your riff</span>
 
                       <GrTwitter
-                        className="ml-4 w-8 h-8 mb-1 text-[#1DA1F2]"
+                        className="ml-4 w-8 h-8 mb-1 text-[#1DA1F2] hover:text-[#31739d] cursor-pointer"
                         onClick={() => {
                           console.log("click");
                         }}
                       />
                       <GrReddit
-                        className="ml-4 w-8 h-8 mb-1 text-[#FF4500]"
+                        className="ml-4 w-8 h-8 mb-1 text-[#FF4500] hover:text-[#b76344] cursor-pointer"
                         onClick={() => {
                           window.open(getRedditLink(), "_blank");
                         }}
