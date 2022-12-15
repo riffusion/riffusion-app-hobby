a8c0faaa62198c3a7694b4391f4fb3e9b73d834b
Hayk Martiros <hayk.mart@gmail.com>
2022-12-14 23:45:09 -0800

    try explicit image URL

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -5,14 +5,20 @@ export default function PageHead() {
     <Head>
       <title>Riffusion</title>
       <meta property="og:site_name" content="Riffusion" />
-      <meta
-        property="og:title"
-        content="Riffusion - Stable diffusion for real-time music generation"
-      />
+
       <meta
         property="article:author"
         content="Seth Forsgren and Hayk Martiros"
       />
+
+      <meta property="article:tag" content="music" />
+      <meta property="article:tag" content="machine learning" />
+      <meta property="article:tag" content="artificial intelligence" />
+      <meta property="article:tag" content="generative music" />
+
+      <meta property="og:title" content="Riffusion" />
+      <meta name="twitter:title" content="Riffusion" />
+
       <meta
         name="description"
         content="Stable diffusion for real-time music generation"
@@ -21,17 +27,21 @@ export default function PageHead() {
         name="og:description"
         content="Stable diffusion for real-time music generation"
       />
+      <meta
+        name="twitter:description"
+        content="Stable diffusion for real-time music generation"
+      />
+
+      <meta name="twitter:card" content="summary" />
+
+      <meta property="og:image" content="https://i.imgur.com/fywZpQ7.jpeg" />
+      <meta name="twitter:image" content="https://i.imgur.com/fywZpQ7.jpeg" />
+
+      <meta property="og:type" content="website" />
+
       <meta property="og:url" content="http://www.riffusion." />
       <meta property="og:locale" content="en_US" />
       <meta property="og:website" content="http://wwww.riffusion" />
-      <meta property="og:image" content="/public/pop_sample.jpg" />
-
-      <meta name="twitter:card" content="summary" />
-      <meta
-        name="twitter:title"
-        content="Stable diffusion for real-time music generation"
-      />
-      <meta name="twitter:image" content="/public/pop_sample.jpg" />
 
       <link rel="icon" href="/favicon.ico" />
     </Head>
