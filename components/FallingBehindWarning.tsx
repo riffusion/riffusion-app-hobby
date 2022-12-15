21440dcdb2cb0c8bd0f8b6c0f46f66b110e770b4
Hayk Martiros <hayk.mart@gmail.com>
2022-12-14 23:33:45 -0800

    fix falling behind sizing

--- components/FallingBehindWarning.tsx
+++ components/FallingBehindWarning.tsx
@@ -1,7 +1,7 @@
 export default function FallingBehindWarning() {
   return (
     <div
-      className="fixed z-20 top-8 md:bottom-8 left-12 md:left-12 bg-yellow-50 border border-yellow-600 text-yellow-700 px-4 py-3 rounded"
+      className="fixed z-20 top-8 left-12 bg-yellow-50 border border-yellow-600 text-yellow-700 px-4 py-3 rounded"
       role="alert"
     >
       <span className="block sm:inline mr-8">
