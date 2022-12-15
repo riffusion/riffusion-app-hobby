fd53bc14997c2915171c0a5a65cd46602bb439df
Hayk Martiros <hayk.mart@gmail.com>
2022-12-14 23:32:42 -0800

    add twitter card

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -24,8 +24,14 @@ export default function PageHead() {
       <meta property="og:url" content="http://www.riffusion." />
       <meta property="og:locale" content="en_US" />
       <meta property="og:website" content="http://wwww.riffusion" />
-      <meta name="twitter:image" content="/public/pop_sample.jpg" />
       <meta property="og:image" content="/public/pop_sample.jpg" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta
+        name="twitter:title"
+        content="Stable diffusion for real-time music generation"
+      />
+      <meta name="twitter:image" content="/public/pop_sample.jpg" />
 
       <link rel="icon" href="/favicon.ico" />
     </Head>
