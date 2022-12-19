2c163cae9c6153c1cc06b6cf306a168af5f66a24
Hayk Martiros <hayk.mart@gmail.com>
2022-12-18 22:18:38 -0800

    Update local use timeout from 15 to 60 seconds

--- pages/api/server.js
+++ pages/api/server.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     method: "POST",
     headers: headers,
     body: req.body,
-    signal: AbortSignal.timeout(15000),
+    signal: AbortSignal.timeout(60000),
   });
 
   const data = await response.json();
